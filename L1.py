2f12985dae70a47cae8ca02fff77da750e7fdf3a
Rsingh230105 <rajendra.singh@cdgi.edu.in>
2025-06-23 22:00:22 +0530

    Add files via upload

---
+++ L1.py
@@ -0,0 +1,3 @@
+fruits=["mango","grapes","banana","coconut"]
+#print(fruits[int(input("Enter index for fruits:"))])
+print(fruits[2])
---
+++ L4.py
@@ -0,0 +1,14 @@
+l=[10,30,40,50,"Hello",[1,2,3]]
+print(type(l))#list
+
+print(l[3])#50
+
+print(l[2:])#[40,50,'Hello',[1,2,3]]
+
+print(l[0::2])#[10,40,'Hello']
+
+print(l[5][2])#3
+
+print(l[-1])#[[1,2,3]]
+
+print(l[-1::-1])
